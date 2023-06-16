a88343140f0e4372165694e802d6a6293f4856f4
Thomas PRADEAU <pradeau.thomas0@gmail.com>
2023-06-16 11:51:04 +0200

    fix: feeds and articles migrations

--- database/migrations/2022_03_09_124327_create_articles_table.php
+++ database/migrations/2022_03_09_124327_create_articles_table.php
@@ -18,13 +18,11 @@ class CreateArticlesTable extends Migration
             $table->string('title');
             $table->mediumText('description');
             $table->mediumText('link');
-            $table->string('category')->nullable()->default('uncategorized');
             $table->string('pubdate');
-            $table->string('locale')->default('null');
+            $table->string('locale')->default('undetermined');
             $table->string('static_hash')->default("nostatichash");
             $table->string('dynamic_hash')->default("nodynamichash");
             $table->timestamps();
-            $table->timestamp('published_at')->nullable()->useCurrent();
             $table->unsignedInteger('feed_id');
             $table->foreign('feed_id')->references('id')->on('feeds')->onUpdate('cascade')->onDelete('cascade');
         });
